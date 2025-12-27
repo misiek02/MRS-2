308e6d1c19f48f6f1be9fb418ed6b463a7bbd6b8
danjayy <lifteddebbymartha@gmail.com>
2025-12-27 10:34:19 +0100

    feat: added functions for offset matrix formation with ODD number of robots (for mission)

--- mrs_consensus_improved/mrs_consensus_improved/utils/helper.py
+++ mrs_consensus_improved/mrs_consensus_improved/utils/helper.py
@@ -257,3 +257,181 @@ def get_formation_offset_matrix(desired_shape:str, num_robots:int, vleader_pos:l
 
         case _:
             return None # invalid string provided for desired_shape
+        
+
+def get_formation_offset_matrix_mission(desired_shape:str, num_robots:int, vleader_pos:list = [0.0, 0.0], spacing=0.7):
+    """
+        Function to create the offset matrix for formation control.
+        Note that for this function, the nparity of the num_robots doesn't matter.
+
+        Parameters:
+            desired_shape: (str) string indicating the desired formation shape. Check params file for details
+            num_robots: (int) number of robots partaking in formation
+            vleader_pos: (list) 2D list of virtual leader initial position. Default is [0.0, 0.0]
+            spacing: (float) minimum distance between robots in formation 
+
+        Returns:
+            offsets: (np.ndarray) Nx2 array of robot offsets representing their desired formation (wrt to the virtual leader position).
+    """
+    offsets = [np.zeros((2,))] * num_robots
+
+    if num_robots % 2 == 0: # even number of robots
+        match desired_shape:
+            case 'Lh':
+                # NOTE: Horizontal formation is arranged on y axis
+                half_num_robots = num_robots // 2
+
+                # Assign positions for first half of the line
+                for i in range(int(num_robots/2)):
+                    offsets[i] = np.array([vleader_pos[0], vleader_pos[1]+((half_num_robots-1)*spacing)])
+                    half_num_robots -= 1
+                # Assign positions for second half of the line
+                half_num_robots = num_robots // 2
+                for i in range(int(num_robots/2), num_robots):
+                    offsets[i] = np.array([vleader_pos[0], vleader_pos[1]-(half_num_robots*spacing)])
+                    half_num_robots -= 1
+
+                return np.array(offsets)
+
+            case 'Lv':
+                # NOTE: Vertical formation is arranged on x axis
+                half_num_robots = num_robots // 2
+
+                # Assign positions for first half of the line
+                for i in range(int(num_robots/2)):
+                    offsets[i] = np.array([vleader_pos[0]+((half_num_robots-1)*spacing), vleader_pos[1]])
+                    half_num_robots -= 1
+                # Assign positions for second half of the line
+                half_num_robots = num_robots // 2
+                for i in range(int(num_robots/2), num_robots):
+                    offsets[i] = np.array([vleader_pos[0]-(half_num_robots*spacing), vleader_pos[1]])
+                    half_num_robots -= 1
+
+                return np.array(offsets)
+
+            case 'T':   # valid only for 4 and 6 number of robots (clockwise arrangement starting from top triangle vertex)
+                if num_robots == 4:
+                    offsets[0] = np.array([vleader_pos[0]+spacing, vleader_pos[1]])
+                    offsets[1] = np.array([vleader_pos[0]-spacing, vleader_pos[1]-spacing])
+                    offsets[2] = np.array([vleader_pos[0]-spacing, vleader_pos[1]])
+                    offsets[3] = np.array([vleader_pos[0]-spacing, vleader_pos[1]+spacing])
+                elif num_robots == 6: # equilateral triangle
+                    L = spacing * 2     # length of one side
+                    h = (np.sqrt(3)*L)/2    # triangle height
+                    offsets[0] = np.array([vleader_pos[0]+(h/2), vleader_pos[1]])
+                    x_spacing = spacing * np.cos(np.pi/6)
+                    y_spacing = spacing * np.sin(np.pi/6)
+                    offsets[1] = np.array([vleader_pos[0]+(h/2)-x_spacing, vleader_pos[1]-y_spacing])
+                    offsets[2] = np.array([vleader_pos[0]-(h/2), vleader_pos[1]-(L/2)])
+                    offsets[3] = np.array([vleader_pos[0]-(h/2), vleader_pos[1]])
+                    offsets[4] = np.array([vleader_pos[0]-(h/2), vleader_pos[1]+(L/2)])
+                    offsets[5] = np.array([vleader_pos[0]+(h/2)-x_spacing, vleader_pos[1]+y_spacing])
+                else:
+                    return 
+
+                # Return the offset positions
+                return np.array(offsets)
+
+            case _:
+                return None # invalid string provided for desired_shape
+            
+    else:   # num_robots is odd
+        match desired_shape:
+            case 'Lh':
+                # NOTE: Horizontal formation is arranged on y axis
+                half_num_robots = (num_robots-1) // 2
+
+                # Assign positions for first half of the line
+                for i in range(int((num_robots-1)/2)):
+                    offsets[i] = np.array([vleader_pos[0], vleader_pos[1]+(half_num_robots*spacing)])
+                    half_num_robots -= 1
+                # Assign positions for second half of the line
+                half_num_robots = (num_robots-1) // 2
+                for i in range(int((num_robots-1)/2), num_robots-1):
+                    offsets[i] = np.array([vleader_pos[0], vleader_pos[1]-(half_num_robots*spacing)])
+                    half_num_robots -= 1
+                # Assign position for the last ('odd') robot
+                offsets[num_robots-1] = np.array([vleader_pos[0], vleader_pos[1]])
+                return np.array(offsets)
+
+            case 'Lv':
+                # NOTE: Vertical formation is arranged on x axis
+                half_num_robots = (num_robots-1) // 2
+
+                # Assign positions for first half of the line
+                for i in range(int((num_robots-1)/2)):
+                    offsets[i] = np.array([vleader_pos[0]+(half_num_robots*spacing), vleader_pos[1]])
+                    half_num_robots -= 1
+                # Assign positions for second half of the line
+                half_num_robots = (num_robots-1) // 2
+                for i in range(int((num_robots-1)/2), num_robots-1):
+                    offsets[i] = np.array([vleader_pos[0]-(half_num_robots*spacing), vleader_pos[1]])
+                    half_num_robots -= 1
+                # Assign position for the last ('odd') robot
+                offsets[num_robots-1] = np.array([vleader_pos[0], vleader_pos[1]])
+                return np.array(offsets)
+
+            case 'T':   # valid only for 3 number of robots (clockwise arrangement starting from top triangle vertex)
+                if num_robots == 3:
+                    offsets[0] = np.array([vleader_pos[0]+spacing, vleader_pos[1]])
+                    offsets[1] = np.array([vleader_pos[0]-spacing, vleader_pos[1]-spacing])
+                    offsets[2] = np.array([vleader_pos[0]-spacing, vleader_pos[1]+spacing])
+
+
+def get_formation_offset_matrix_square(desired_shape:str, num_robots:int, vleader_pos:list = [0.0, 0.0], spacing=0.7):
+    """
+        Function to create the offset matrix for SQUARE formation control.
+        Note that this function assumes: the number of robots in the formation is divisible by 4.
+
+        Parameters:
+            desired_shape: (str) string indicating the desired formation shape
+            num_robots: (int) number of robots partaking in formation
+            vleader_pos: (list) 2D list of virtual leader initial position. Default is [0.0, 0.0]
+            spacing: (float) minimum distance between robots in formation 
+
+        Returns:
+            offsets: (np.ndarray) Nx2 array of robot offsets
+    """
+    assert num_robots % 4 == 0  # check that the number of robots is divisible by 4
+    offsets = []  # Use empty list, append as we go
+
+    if desired_shape == 'S':
+        robots_per_side = num_robots // 4
+        
+        # Calculate the side length and half-side for centering
+        side_length = (robots_per_side - 1) * spacing
+        half_side = side_length / 2.0
+        
+        robot_idx = 0
+        
+        # Top side (left to right)
+        y = vleader_pos[1] + half_side
+        for i in range(robots_per_side):
+            x = vleader_pos[0] - half_side + i * spacing
+            offsets.append(np.array([x, y]))
+            robot_idx += 1
+        
+        # Right side (top to bottom)
+        x = vleader_pos[0] + half_side
+        for i in range(robots_per_side):
+            y = vleader_pos[1] + half_side - i * spacing
+            offsets.append(np.array([x, y]))
+            robot_idx += 1
+        
+        # Bottom side (right to left)
+        y = vleader_pos[1] - half_side
+        for i in range(robots_per_side):
+            x = vleader_pos[0] + half_side - i * spacing
+            offsets.append(np.array([x, y]))
+            robot_idx += 1
+        
+        # Left side (bottom to top)
+        x = vleader_pos[0] - half_side
+        for i in range(robots_per_side):
+            y = vleader_pos[1] - half_side + i * spacing
+            offsets.append(np.array([x, y]))
+            robot_idx += 1
+    else:
+        return None
+    
+    return np.array(offsets)
